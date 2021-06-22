ef57928598ddacb148106b1c146f29aadc99dd91
Elviin Thomas Eldho <elviin.t.eldho@gmail.com>
2021-06-23 00:27:33 +0530

    version 1.16.4 : database url toggle

--- Social_Vision_Web/settings.py
+++ Social_Vision_Web/settings.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 import psycopg2
 
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,12 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'qwwbcq@xc*td8)va0h&tvblldl0ks8cdp_mn$q31@tyj+zl36-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-if DEBUG == "False" :
-    DATABASE_URL = os.environ['DATABASE_URL']
-
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+DEBUG = False
 
 ALLOWED_HOSTS = ['social-vision.herokuapp.com', '127.0.0.1']
 
